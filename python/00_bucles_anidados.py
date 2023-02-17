c3b83c1f66a2b191480e51de30ea3cba3149163a
angelflorentino <angel.florentino.00127@maristascomayagua.edu.hn>
2023-02-17 16:45:28 -0600

    Documentación para el 00_bucles_anidados

--- python/00_bucles_anidados.py
+++ python/00_bucles_anidados.py
@@ -4,10 +4,10 @@
 
 n = int(input("Introduzca un número N: "))
 
-for i in range(n):
-    for i2 in range(i+1):
-        print("*", end=" ")
-    print("")
+for i in range(n): # este bucle sirve para iterar la cantidad de veces indicada por el usuario.
+    for i2 in range(i+1):   # este bucle iterará hasta el valor almacenado en i, aumentado en 1 por la resta que hace el for.
+        print("*", end=" ") # esta instrucción imprimirá el asterisco sin salto de línea, la cantidad de veces que valga i.
+    print("")   # aquí se hará el salto de línea, cada vez que termine de iterar el bucle i2.
 
 # impresión de un triángulo rectángulo con números que vuelven a comenzar por cada fila.
 
@@ -15,7 +15,7 @@ n = int(input("Introduzca un número N: "))
 
 for i in range(n):
     for i2 in range(i+1):
-        print(i2+1, end=" ")
+        print(i2+1, end=" ")    # este es el cambio acá, se imprime i2+1, que cada vez que finaliza se formatea a 0, y llega hasta el valor que se tenga de i.
     print("")
 
 # impresión de un triángulo rectángulo cuyas filas son el número de la iteración.
@@ -24,5 +24,5 @@ n = int(input("Introduzca un número N: "))
 
 for i in range(n+1):
     for i2 in range(i):
-        print(i, end=" ")
+        print(i, end=" ")   # acá se imprime i, que corresponde también al número de elementos que se imprimirán, el sub-bucle imprime i, i veces.
     print("")
